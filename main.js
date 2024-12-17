d10df49c44a5162f61445a122ee5b684e7afa00d
Rasulbek <quvondiqovrasulbek054@gmail.com>
2024-12-17 22:35:51 +0500

    exit

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ let obj = [];
 
 function userEnter(){
   let userInput = prompt("Buyruq kiriting (add massivga qoshish `del` o'chirish `stop` to'htatish)");
-
+  
+    
   if (userInput == 'stop') {
     console.log(`yakunlandi`,obj);
     return;
@@ -15,20 +16,28 @@ function userEnter(){
   
   let war = parts[1]; 
 
-  
+  if(num=="add" || num=="del"){
   if (num == 'add' ) {
     obj.push(war);
    
     console.log(obj);
   }
   else if (num == `del`){
-    obj.splice(0,1);
+    job= +prompt("nechanchi elementni o'chirmoqchi siz (Elementlar tartibi 0 dan boshlab)")
+
+    obj.splice(job,job++);
     console.log(obj);
   }
    else {
     
    alert("Nimadur hato ketdi");
   }
+  
+}
+else{
+  alert("Iltimos buyruqni to'g'ri kiriting!")
+}
+
   userEnter();
 }
 
