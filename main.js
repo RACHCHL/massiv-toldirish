8993e8f9cee015817d8fff4e0b645feabf07162b
Rasulbek <quvondiqovrasulbek054@gmail.com>
2024-12-17 17:32:13 +0500

    main

---
+++ main.js
@@ -0,0 +1,35 @@
+let obj = [];
+
+
+function userEnter(){
+  let userInput = prompt("Buyruq kiriting (add massivga qoshish `del` o'chirish `stop` to'htatish)");
+
+  if (userInput == 'stop') {
+    console.log(`yakunlandi`,obj);
+    return;
+  }
+
+  let parts = userInput.split(",");
+  
+  let num = parts[0]; 
+  
+  let war = parts[1]; 
+
+  
+  if (num == 'add' ) {
+    obj.push(war);
+   
+    console.log(obj);
+  }
+  else if (num == `del`){
+    obj.splice(0,1);
+    console.log(obj);
+  }
+   else {
+    
+   alert("Nimadur hato ketdi");
+  }
+  userEnter();
+}
+
+userEnter();
